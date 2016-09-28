47ddcc0f1ea28152f10090dd0a09db32fa989ace
shaaafi <007shafi2007@gmail.com>
2016-09-29 01:08:43 +0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,22 +2,22 @@ var main = function(){
     $(".icon-menu").click(function(){
         $(".menu").animate({
             left:'0px'
-        },500);
+        },200);
         
         $("body").animate({
             left:'285px'
-        },500);
+        },200);
     
     });
     
     $(".icon-close").click(function(){
         $(".menu").animate({
             left:'-285px'
-        },500);
+        },200);
        
         $("body").animate({
             left:'0px'
-        },500);
+        },200);
     });
 };
 
